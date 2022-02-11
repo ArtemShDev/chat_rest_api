19fdc928d6a1b892ba9a714e6cb59e01d6a0308e
ArtemShDev <dev.art.balt@gmail.com>
2022-02-11 13:06:14 +0300

    Создание чата на Rest API

--- src/test/java/org/exchange/springboot/chatrest/controller/RoleRESTControllerTest.java
+++ src/test/java/org/exchange/springboot/chatrest/controller/RoleRESTControllerTest.java
@@ -1,97 +1,75 @@
-/*
+
 package org.exchange.springboot.chatrest.controller;
 
+import org.exchange.springboot.chatrest.entity.Role;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.client.ClientHttpRequest;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
 
 import java.io.*;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class RoleRESTControllerTest {
+
+    @Autowired
+    RoleRESTController roleRESTController;
 
     @Test
     void whenGETRolesThenStatusIsOK() throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
-        ClientHttpRequest req = restTemplate.getRequestFactory()
-                .createRequest(URI.create("http://localhost:8080/api/roles/"), HttpMethod.GET);
-        ClientHttpResponse resp = req.execute();
+        ResponseEntity<List<Role>> roleResponseEntity = roleRESTController.findAll();
         assertThat(
-                resp.getStatusCode(),
+                roleResponseEntity.getStatusCode(),
                 equalTo(HttpStatus.OK));
     }
 
     @Test
     void whenPOSTRoleThenStatusIsOK() throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
-        String json = "{\"name\": \"Unique_Json\"}";
-        ClientHttpRequest req = restTemplate.getRequestFactory()
-                .createRequest(URI.create("http://localhost:8080/api/roles/"), HttpMethod.POST);
-        req.getBody().write(json.getBytes());
-        req.getHeaders().add("content-type", "application/json");
-        ClientHttpResponse resp = req.execute();
+        Role role = new Role("TEST_ROLE");
+        ResponseEntity<Role> roleResponseEntity = roleRESTController.create(role);
         assertThat(
-                resp.getStatusCode(),
+                roleResponseEntity.getStatusCode(),
+                equalTo(HttpStatus.OK));
+        assertTrue(roleResponseEntity.getBody().getId() != 0);
+    }
+
+    @Test
+    void whenGETRoleByIDThenOK() throws IOException {
+        ResponseEntity<Role> roleResponseEntity = roleRESTController.create(new Role("TEST_ROLE"));
+        ResponseEntity<Role> role2ResponseEntity = roleRESTController.findById(roleResponseEntity.getBody().getId());
+        assertThat(
+                role2ResponseEntity.getStatusCode(),
                 equalTo(HttpStatus.OK));
     }
 
     @Test
-    void whenPOSTRoleAndDeleteRoleThenStatusIsOK() throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
-        ClientHttpRequest req = restTemplate.getRequestFactory()
-                .createRequest(URI.create("http://localhost:8080/api/roles/"), HttpMethod.POST);
-        req.getBody().write("{\"name\": \"Unique_Json\"}".getBytes(StandardCharsets.UTF_8));
-        req.getHeaders().add("content-type", "application/json");
-        ClientHttpResponse resp = req.execute();
-        String str = new String(resp.getBody().readAllBytes());
-        Pattern pattern = Pattern.compile("id\":\\d+");
-        Matcher matcher = pattern.matcher(str);
-        if (matcher.find()) {
-            str = matcher.group();
-            req = restTemplate.getRequestFactory()
-                    .createRequest(URI.create("http://localhost:8080/api/roles/" + Integer.parseInt(str.substring(4))),
-                             HttpMethod.DELETE);
-            resp = req.execute();
-            assertThat(
-                    resp.getStatusCode(),
-                    equalTo(HttpStatus.OK));
-        }
+    void whenDELETERoleThenStatusIsOK() throws IOException {
+        ResponseEntity<Role> roleResponseEntity = roleRESTController.create(new Role("TEST_ROLE"));
+        Role role = roleResponseEntity.getBody();
+        ResponseEntity<Void> voidResponseEntity = roleRESTController.delete(role.getId());
+        assertThat(
+                voidResponseEntity.getStatusCode(),
+                equalTo(HttpStatus.OK));
     }
 
-    /*
     @Test
-    void whenGETRolesThenPayloadIsOK() throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
-        ClientHttpRequest req = restTemplate.getRequestFactory()
-                .createRequest(URI.create("http://localhost:8080/api/roles/"), HttpMethod.GET);
-        req.getHeaders().add("content-type", "application/json");
-        ClientHttpResponse resp = req.execute();
-        String str = new String(resp.getBody().readAllBytes());
-        assertTrue(str.contains("\"id\":1"));
+    void whenPUTRoleThenStatusIsOK() throws IOException {
+        ResponseEntity<Role> roleResponseEntity = roleRESTController.create(new Role("TEST_ROLE"));
+        Role role = roleResponseEntity.getBody();
+        role.setName("NEW_TEST_ROLE");
+        ResponseEntity<Void> voidResponseEntity = roleRESTController.update(role);
+        assertThat(
+                voidResponseEntity.getStatusCode(),
+                equalTo(HttpStatus.OK));
+        assertEquals(role.getName(), roleRESTController.findById(role.getId()).getBody().getName());
     }
-     */
-/*
-    @Test
-    void whenGETRoleByIDThenPayloadIsOK() throws IOException {
-        RestTemplate restTemplate = new RestTemplate();
-        ClientHttpRequest req = restTemplate.getRequestFactory()
-                .createRequest(URI.create("http://localhost:8080/api/roles/1"), HttpMethod.GET);
-        ClientHttpResponse resp = req.execute();
-        String str = new String(resp.getBody().readAllBytes());
-        assertTrue(str.contains("\"id\":1"));
-    }
+
 }
-*/
